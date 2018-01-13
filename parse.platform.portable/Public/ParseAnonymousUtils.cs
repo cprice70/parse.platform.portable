bc4d9a788c3dc8510bb0ff893fe7b5db8ae7956f
cprice70 <cprice@amplextech.com>
2018-01-12 20:57:43 -0500

    Initial commit

---
+++ parse.platform.portable/Public/ParseAnonymousUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parse.Core.Public
+{
+    /// <summary>
+    /// Parse anonymous utils.
+    /// </summary>
+    public static class ParseAnonymousUtils
+    {
+        /// <summary>
+        /// The type of the auth.
+        /// </summary>
+        private const string AUTH_TYPE = "anonymous";
+
+        /// <summary>
+        /// Whether the user is logged in anonymously
+        /// 
+        /// </summary>
+        public static bool IsLinked(this ParseUser user)
+        {
+            return user.IsLinked(AUTH_TYPE);
+        }
+
+        /// <summary>
+        /// Logs the in async.
+        /// </summary>
+        /// <returns>The in async.</returns>
+        /// <param name="token">Token.</param>
+        public static Task<ParseUser> LogInAsync(CancellationToken token)
+        {
+            return ParseUser.LogInAsync(AUTH_TYPE, GetAuthData(), token);
+        }
+
+        /// <summary>
+        /// Gets the auth data.
+        /// </summary>
+        /// <returns>The auth data.</returns>
+        private static Dictionary<string, object> GetAuthData()
+        {
+            return new Dictionary<string, object>
+            {
+                { "id", Guid.NewGuid().ToString() }
+            };
+        }
+
+    }
+}
